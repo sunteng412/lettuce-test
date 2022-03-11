79eccf4d53f207b400316416d7c937fecfe4e98f
龙川 <longchuan.stt@raycloud.com>
2022-03-11 17:13:56 +0800

    feat：新增监控

---
+++ src/main/java/com/lingxuan/lettucetest/WatchNettyDirectMemory.java
@@ -0,0 +1,51 @@
+package com.lingxuan.lettucetest;
+
+import cn.hutool.core.date.LocalDateTimeUtil;
+import cn.hutool.core.io.FileUtil;
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 监听堆外
+ *
+ * @author : longchuan
+ * @version :
+ * @date : 2022/3/11 3:59 下午
+ * @description:
+ */
+public class WatchNettyDirectMemory {
+
+    static File file = new File(String.format("/Users/admin/ideaProject/test/lettuce-test/jvm/jstack-5.1.4.log",
+            LocalDateTimeUtil.format(LocalDateTime.now(),"")));
+
+    static {
+        boolean del = FileUtil.del(file);
+        System.out.println(del);
+    }
+
+    /**
+     * 监听
+     *
+     * @param
+     * @return
+     * @description:
+     */
+    public static void watch(int capacity, AtomicLong directMemory) {
+        System.out.println("堆外内存：新增：" + capacity + " bytes,目前大小：" + directMemory.get() + " bytes");
+        if(capacity > 200000){
+            jstack(capacity,directMemory);
+        }
+    }
+
+
+    public static String jstack(int capacity, AtomicLong directMemory) {
+        String jstack = VMUtil.jstack() + String.format("\n----------新增：%s-------目前：%s----------------------------\n", capacity,directMemory);
+        FileUtil.appendUtf8String(jstack,file);
+        return jstack;
+    }
+
+}
+
--- src/test/java/com/lingxuan/lettucetest/ChatListenerTest.java
+++ src/test/java/com/lingxuan/lettucetest/ChatListenerTest.java
@@ -1,7 +1,12 @@
 package com.lingxuan.lettucetest;
 
 
+import com.google.common.base.Throwables;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
 import lombok.SneakyThrows;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +22,28 @@ import java.util.concurrent.TimeUnit;
 @SpringBootTest
 public class ChatListenerTest {
 
+    @BeforeClass
+    public static void init() {
+        try {
+            ClassPool cp = ClassPool.getDefault();
+            CtClass cc = cp.get("io.netty.util.internal.PlatformDependent");
+            //解冻
+            cc.defrost();
+            CtMethod m = cc.getDeclaredMethod("incrementMemoryCounter");
+            //m.insertBefore("{ System.out.println(\"incrementMemoryCounter start!!!\"); }");
+            //m.insertAfter("{System.out.println(\"堆外内存：新增：\" +capacity + \" bytes,目前大小：\" + DIRECT_MEMORY_COUNTER.get() + \" bytes\"); }");
+            m.insertAfter("com.lingxuan.lettucetest.WatchNettyDirectMemory.watch(capacity,DIRECT_MEMORY_COUNTER);");
+
+            //m.insertAfter("{ System.out.println(\"incrementMemoryCounter end!!!\"); }");
+            cc.writeFile("/Users/admin/ideaProject/test/lettuce-test/classes");
+            //cc.writeFile();
+            cc.toClass(ChatListenerTest.class.getClassLoader());
+        } catch (Exception e) {
+            System.out.println(Throwables.getStackTraceAsString(e));
+        }
+
+    }
+
     @Autowired
     private CacheTest cacheTest;
 
@@ -25,24 +52,21 @@ public class ChatListenerTest {
     @Test
     public void test() {
 
+        System.out.println("开始");
+
         ExecutorService executorService = Executors.newFixedThreadPool(10);
 
         for (int i = 0; i < 4; i++) {
-
-            executorService.execute(()->{
-                System.out.println(cacheTest.selectUrgeSettingsList(100003098L,10).size());;
+            executorService.execute(() -> {
+                System.out.println(cacheTest.selectUrgeSettingsList(100003098L, 10).size());
             });
         }
 
-        TimeUnit.SECONDS.sleep(10);
-
-        for (int i = 0; i < 4; i++) {
-
-            executorService.execute(()->{
-                System.out.println(cacheTest.selectUrgeSettingsList(100003098L,10).size());;
-            });
-        }
-
+//        for (int i = 0; i < 4; i++) {
+//            executorService.execute(() -> {
+//                System.out.println(cacheTest.selectUrgeSettingsList(100003098L, 10).size());
+//            });
+//        }
         System.in.read();
     }
 }
